5327b6f5c833ab412daaf0e9d7ec9499f2a12e5b
ShivaniDixit05 <shivanidixit0525@gmail.com>
2025-09-27 13:42:52 +0530

    Update vari.cpp

--- vari.cpp
+++ vari.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+using namespace std;
 
+int main()
+{
+    //integer 
+    int age;
+    age = 36;
+    cout << age << endl;
+
+    //Char
+    char grade;
+    grade = 'a';
+    cout << grade << endl;
+
+    // float
+    float num;
+    num = 345.987646f;
+    cout << num << endl;
