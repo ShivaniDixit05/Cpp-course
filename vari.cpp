c3e598c7abbb3bfd73ec51bc4f06587f1eb04ea8
ShivaniDixit05 <shivanidixit0525@gmail.com>
2025-09-27 13:43:12 +0530

    Update vari.cpp

--- vari.cpp
+++ vari.cpp
@@ -17,3 +17,17 @@ int main()
     float num;
     num = 345.987646f;
     cout << num << endl;
+
+    // bool  true -> 1 and false -> 0
+    bool isSafe;
+    isSafe = false;
+    cout << isSafe << endl;
+
+    // double
+    double Num;
+    Num = 9876.987654321;
+    cout << Num << endl;
+
+    return 0;
+
+}
