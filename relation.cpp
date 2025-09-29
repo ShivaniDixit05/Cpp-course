2e8ac8d184aeef524b158205ad1055a850aabd7a
ShivaniDixit05 <shivanidixit0525@gmail.com>
2025-09-29 21:05:46 +0530

    Update relation.cpp

--- relation.cpp
+++ relation.cpp
@@ -1,1 +1,13 @@
+#include <iostream>
+using namespace std;
 
+int main()  
+{ 
+    cout << (3<5) << endl; //true -> 1
+    cout << (3>5) << endl; // false -> 0
+    cout << (3==3) << endl; 
+    cout << (3<=5) << endl; 
+    cout << (3>=5) << endl; 
+    cout << (3!=5) << endl; 
+    return 0;
+}
