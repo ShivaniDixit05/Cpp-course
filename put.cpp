0d019fa318a7be4961ee74ab7bb57f2a6c1a554a
ShivaniDixit05 <shivanidixit0525@gmail.com>
2025-09-30 16:29:21 +0530

    Update put.cpp

--- put.cpp
+++ put.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+using namespace std;
 
+int main()
+{
+    double price;
+    cout << "Enter your price : ";
+    cin >> price;
+    cout << "Your price is : " << price << endl;
+
+    int age;
+    cout << "Enter your age : ";
+    cin >> age;
+    cout << "Your age is : " << age << endl;
+
+    return 0;
+}
